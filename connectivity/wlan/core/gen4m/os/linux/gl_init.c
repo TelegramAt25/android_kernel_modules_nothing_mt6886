961fb9bb87c0c45aef2feb53eaa82517ae06e2a2
Rachel Tseng <Rachel.Tseng@mediatek.com>
2023-06-13 11:40:24 +0800

    [ALPS07870947] wlan: support get mlo link count and tdls session
    Support vendor HAL API:
    - getMaxMloAssociationLinkCount
    - getMaxMloStrLinkCount
    - getMaxSupportedConcurrentTdlsSession
    
    MTK-Commit-Id: 185d3620df2790abd49a427ee0d7ad059b3ac99a
    
    Change-Id: Ibf61750f7d116cc817d56986a5126721a115bf40
    CR-Id: ALPS07870947
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Rachel Tseng <Rachel.Tseng@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7566452
    Test: srv_pf_nep_sanity
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: rachel.tseng <rachel.tseng@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_pis <srv_pis@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Tested-by: rachel.tseng <rachel.tseng@mediatek.com>
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    Reviewed-by: zeno.yang <zeno.yang@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- connectivity/wlan/core/gen4m/os/linux/gl_init.c
+++ connectivity/wlan/core/gen4m/os/linux/gl_init.c
@@ -1655,6 +1655,18 @@ static const struct wiphy_vendor_command
 		.maxattr = QCA_WLAN_VENDOR_ATTR_P2P_LO_MAX
 	},
 #endif /* CFG_SUPPORT_P2P_LISTEN_OFFLOAD */
+	/* Get Chip Capabilities From Wifi Driver */
+	{
+		{
+			.vendor_id = OUI_MTK,
+			.subcmd = MTK_SUBCMD_GET_CHIP_CAPABILITIES
+		},
+		.flags = WIPHY_VENDOR_CMD_NEED_WDEV |
+				WIPHY_VENDOR_CMD_NEED_NETDEV,
+		.doit = mtk_cfg80211_vendor_get_chip_capabilities,
+#if KERNEL_VERSION(5, 4, 0) <= LINUX_VERSION_CODE
+		.policy = VENDOR_CMD_RAW_DATA,
+#endif
 };
 
 static const struct nl80211_vendor_cmd_info
