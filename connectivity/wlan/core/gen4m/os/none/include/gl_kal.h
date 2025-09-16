610e157c97e69e9d753e2d535b96379324f66012
Cyber Knight <cyberknight755@gmail.com>
2025-09-16 18:37:07 +0800

    gen4m: Fix compilation on !CFG_MET_PACKET_TRACE_SUPPORT
    Change-Id: I46b172950baccd1d292c67d49ae5e2dbf13e5049
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- connectivity/wlan/core/gen4m/os/none/include/gl_kal.h
+++ connectivity/wlan/core/gen4m/os/none/include/gl_kal.h
@@ -1929,8 +1929,10 @@ int rx_thread(void *data);
 uint64_t kalGetBootTime(void);
 #endif
 
+#if CFG_MET_PACKET_TRACE_SUPPORT
 int kalMetInitProcfs(struct GLUE_INFO *prGlueInfo);
 int kalMetRemoveProcfs(void);
+#endif
 
 uint8_t kalGetEapolKeyType(void *prPacket);
 
