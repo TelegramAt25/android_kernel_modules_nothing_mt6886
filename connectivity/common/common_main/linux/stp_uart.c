650a5235a46e7a429bdffe49553e9ca722e85019
fukiame <fukiame@proton.me>
2026-04-18 16:51:40 +0700

    fixup! treewide: guard all instances of MIN() and MAX() defines
    followup of kernel commit e035ca130ff7f5655f7c63caaeacaf0828f85cce "minmax: make generic MIN() and MAX() macros available everywhere"
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- connectivity/common/common_main/linux/stp_uart.c
+++ connectivity/common/common_main/linux/stp_uart.c
@@ -44,8 +44,10 @@
 
 #define HCIUARTSETPROTO        _IOW('U', 200, int)
 
+#ifndef MIN
 #define MAX(a, b)        ((a) > (b) ? (a) : (b))
 #define MIN(a, b)        ((a) < (b) ? (a) : (b))
+#endif
 
 #define PFX                         "[UART] "
 #define UART_LOG_LOUD                 4
